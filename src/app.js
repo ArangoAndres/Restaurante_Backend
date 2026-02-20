fc510531f497c06ba9894de6ec0c48ecc0399fdc
andres4423 <olaya4423@gmail.com>
2026-02-20 14:44:08 -0500

    cors2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import redisRoutes from "./routes/redis.routes.js";
 import cors from "cors";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "*"
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
-
-
 app.use("/api/redis", redisRoutes);
 
 export default app;
