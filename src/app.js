d0c5bd643c07605fdbff26b3464fdba62dee7a4c
andres4423 <olaya4423@gmail.com>
2026-02-20 14:47:17 -0500

    cors3

--- src/app.js
+++ src/app.js
@@ -11,8 +11,12 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("*", cors());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use("/api/redis", redisRoutes);
 
